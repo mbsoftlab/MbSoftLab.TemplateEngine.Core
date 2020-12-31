046625d6f564e96b0bcb055cdf8009f348192aca
mbsoftlab <MusikBeatsSoftwareLab@outlook.de>
2021-01-01 00:25:06 +0100

    Improvements

---
+++ MbSoftlab.TemplateEngine.Core.Demo/Address.cs
@@ -0,0 +1,8 @@
+﻿namespace MbSoftLab.TemplateEngine.Core.Demo
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public string PostCode { get; set; }
+    }
+}
--- MbSoftlab.TemplateEngine.Core.Demo/Person.cs
+++ MbSoftlab.TemplateEngine.Core.Demo/Person.cs
@@ -1,31 +1,13 @@
-﻿using RazorEngineCore;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace MbSoftLab.TemplateEngine.Core.Demo
 {
-    public class Person: TemplateDataModel<Person>
+    public class Person : TemplateDataModel<Person>
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public List<string> Tags { get; set; }
         public Address Address { get; set; }
         public List<Order> Orders { get; set; }
-
-    }
-    public class Address
-    {
-        public string Street { get; set; }
-        public string PostCode { get; set; }
-    }
-    public class Order
-    {
-        public int Id { get; set; }
-        public List<Product> Products { get; set; }
-    }
-    public class Product
-    {
-        public string ProductName { get; set; }
-        public float Price { get; set; }
-
     }
 }
